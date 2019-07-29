f79f03293135d3371c30636320886d3dbe411fb0
PKB2810 <pranay@kiprosh.com>
2019-07-29 21:37:23 +0530

    style changes for landing page

--- App.js
+++ App.js
@@ -6,13 +6,22 @@
  * @flow
  */
 
-import React, { Fragment } from "react";
-import { View } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import LandingPage from "./src/components/LandingPage";
 
+const landingPageStyle = StyleSheet.create({
+  landingPageParent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    width: "100%",
+    height: "100%"
+  }
+});
 const App = () => {
   return (
-    <View>
+    <View style={landingPageStyle.landingPageParent}>
       <LandingPage />
     </View>
   );
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { GoogleSigninButton } from "react-native-google-signin";
 import Header from "../Header";
-const landingPageStyle = StyleSheet.create({
-  landingPageParent: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start"
-  }
-});
+
 class LandingPage extends React.Component {
   render() {
     return (
-      <View style={landingPageStyle.landingPageParent}>
+      <>
         <Header>SnackVote</Header>
         <GoogleSigninButton
           style={{ width: 192, height: 48 }}
           size={GoogleSigninButton.Size.Wide}
           color={GoogleSigninButton.Color.Dark}
         />
-      </View>
+      </>
     );
   }
 }
